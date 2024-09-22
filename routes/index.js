55ae57eb82895c6710f3c7d55f04e17d64431efa
ericklucioh <erickrealoficial@gmail.com>
2024-09-22 20:41:48 -0300

    ativando rotas do  appointments

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-//const appointmentsRoutes = require('./appointmentsRoutes');
+const appointmentsRoutes = require('./appointmentsRoutes');
 //const eventsRoutes = require('./eventsRoutes');
 //const professionalsRoutes = require('./professionalsRoutes');
 const studentsRoutes = require('./studentsRoutes');
 const teachersRoutes = require('./teachersRoutes');
 const usersRoutes = require('./usersRoutes');
 
-// router.use('/appointments', appointmentsRoutes);
+router.use('/appointments', appointmentsRoutes);
 // router.use('/events', eventsRoutes);
 // router.use('/professionals', professionalsRoutes);
 router.use('/students', studentsRoutes);
